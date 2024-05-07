1091b7dc7fb4445a366edb30173523f219d3b8cf
MinKevin <alswkdrb1@naver.com>
2024-05-07 17:11:28 +0900

    feat: 공통 게시판 기본 구조 제작 과정
    commonPost 구현 완료
    commonService, commonRepository를 모든 게시판이 공유
    comment 구현 중

--- src/main/java/com/aptner/v3/board/comment/CommentRepository.java
+++ src/main/java/com/aptner/v3/board/comment/CommentRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    @Query("SELECT c FROM Comment c WHERE c.commonPost.id == :postId")
+    @Query("SELECT c FROM Comment c WHERE c.commonPost.id = :postId")
     List<Comment> findAllByPostId(Long postId);
 }
