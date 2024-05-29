7b48df9e8896d6a76c13bc69ac1a917cac500f1f
ysyoo <hoban4336@gmail.com>
2024-05-29 18:06:25 +0900

    [hotfix/cors] localhost:3000 허용 TEST 코드 허용

--- src/main/java/com/aptner/v3/global/config/CorsConfig.java
+++ src/main/java/com/aptner/v3/global/config/CorsConfig.java
@@ -1,48 +1,29 @@
 package com.aptner.v3.global.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-@Configuration
 public class CorsConfig {
-    @Value("${frontend.url}")
-    private String frontendUrl;
+    public static Customizer<CorsConfigurer<HttpSecurity>> corsFilter(String frontEndUrl) {
+        return cors -> cors
+                .configurationSource(corsConfigurationSource(frontEndUrl));
+    }
 
-    @Bean
-    public CorsFilter corsFilterBean() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    private static CorsConfigurationSource corsConfigurationSource(String frontEndUrl) {
         CorsConfiguration configuration = new CorsConfiguration();
-        configureCors(configuration);
-        source.registerCorsConfiguration("/**", configuration);
-        return new CorsFilter(source);
-    }
-
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilterRegistration(CorsFilter corsFilter) {
-        FilterRegistrationBean<CorsFilter> filterRegistrationBean = new FilterRegistrationBean<>(corsFilter);
-        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return filterRegistrationBean;
-    }
-
-    private void configureCors(CorsConfiguration configuration) {
-        configuration.setAllowCredentials(true);
-        configuration.addAllowedOrigin(frontendUrl);
+        configuration.addAllowedOrigin(frontEndUrl);
         configuration.addAllowedOrigin("localhost:3000");
-        configuration.addAllowedHeader("Authorization");
-        configuration.addAllowedHeader("Content-Type");
-        configuration.addAllowedHeader("Accept");
-        configuration.addAllowedMethod("GET");
-        configuration.addAllowedMethod("POST");
-        configuration.addAllowedMethod("PATCH");
-        configuration.addAllowedMethod("PUT");
-        configuration.addAllowedMethod("DELETE");
-        configuration.addAllowedMethod("OPTIONS");
-        configuration.setMaxAge(3600L);
+        configuration.addAllowedOrigin("localhost");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
+
 }
